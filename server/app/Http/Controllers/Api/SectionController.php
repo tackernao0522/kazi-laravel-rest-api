a3c9a0e2a21824671287389fd05852f95a943b13
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2021-12-07 17:44:48 +0900

    RestAPI Section CRUD Part1

---
+++ server/app/Http/Controllers/Api/SectionController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Section;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+
+class SectionController extends Controller
+{
+    public function sectionIndex()
+    {
+        $sections = Section::latest()->get();
+
+        return response()->json($sections);
+    }
+
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'class_id' => 'required',
+            'section_name' => 'required|unique:sections|max:25'
+        ]);
+
+        Section::insert([
+            'class_id' => $request->class_id,
+            'section_name' => $request->section_name,
+            'created_at' => Carbon::now(),
+        ]);
+
+        return response('Student Section Inserted Successfully');
+    }
+
+    public function sectionEdit($id)
+    {
+        $section = Section::findOrFail($id);
+
+        return response()->json($section);
+    }
+}
